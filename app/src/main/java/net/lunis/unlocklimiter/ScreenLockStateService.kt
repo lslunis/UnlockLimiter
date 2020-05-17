0b68117f58628cda9137269022d054d5934d582f
Luke Stebbing <ls@lunis.net>
2020-05-17 16:11:40 -0400

    add intent logging and fix build

--- app/src/main/java/net/lunis/unlocklimiter/ScreenLockStateService.kt
+++ app/src/main/java/net/lunis/unlocklimiter/ScreenLockStateService.kt
@@ -2,9 +2,11 @@ package net.lunis.unlocklimiter
 
 import android.app.Service
 import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.IBinder
+import android.util.Log
 
 
 class ScreenLockStateService : Service() {
@@ -17,16 +19,19 @@ class ScreenLockStateService : Service() {
                 if (intent == null) {
                     return
                 }
-                when (intent.action) {
-                    Intent.ACTION_SCREEN_ON -> onScreenOn()
-                    Intent.ACTION_SCREEN_OFF -> onScreenOff()
-                }
+                Log.w("ScreenLockStateService", intent.action)
+//                when (intent.action) {
+//                    Intent.ACTION_SCREEN_ON -> onScreenOn()
+//                    Intent.ACTION_SCREEN_OFF -> onScreenOff()
+//                }
 
             }
         }
         val filter = IntentFilter()
         filter.addAction(Intent.ACTION_SCREEN_OFF)
         filter.addAction(Intent.ACTION_SCREEN_ON)
+        filter.addAction(Intent.ACTION_USER_PRESENT)
+        filter.addAction(Intent.ACTION_USER_UNLOCKED)
         registerReceiver(receiver, filter)
     }
 
