e9a898f5cdd9f86023b0ccf849123f60da48dde2
Luke Stebbing <ls@lunis.net>
2020-05-24 14:46:44 -0400

    Start at boot

--- app/src/main/java/net/lunis/unlocklimiter/ScreenLockStateService.kt
+++ app/src/main/java/net/lunis/unlocklimiter/ScreenLockStateService.kt
@@ -9,12 +9,12 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.IBinder
 import android.os.SystemClock.elapsedRealtime
-import android.util.Log
 import androidx.core.app.NotificationCompat
 
 
 class ScreenLockStateService : Service() {
     private lateinit var receiver: BroadcastReceiver
+    private lateinit var nudgeChannel: NotificationChannel
     private var sessionPeriod: Long = 3_600_000
     private var restPeriod: Long = 300_000
     private var nagPeriod: Long = 15_000
@@ -22,15 +22,25 @@ class ScreenLockStateService : Service() {
     private var shouldLockAt: Long = 0
 
     override fun onCreate() {
-        Log.w("ScreenLockStateService", "On create")
         super.onCreate()
-        val channel =
-            NotificationChannel("nudges", "Nudges", NotificationManager.IMPORTANCE_HIGH)
+        val foregroundChannel =
+            NotificationChannel(
+                "foreground",
+                "Unlock Limiter",
+                NotificationManager.IMPORTANCE_LOW
+            )
+
+        nudgeChannel =
+            NotificationChannel("nudge", "Nudges", NotificationManager.IMPORTANCE_HIGH)
         val notificationManager: NotificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-        val foregroundNotification = NotificationCompat.Builder(this, "nudges")
-            .setSmallIcon(android.R.drawable.ic_menu_close_clear_cancel).build()
+        notificationManager.createNotificationChannel(foregroundChannel)
+        notificationManager.createNotificationChannel(nudgeChannel)
+
+
+        val foregroundNotification = NotificationCompat.Builder(this, "foreground")
+            .setSmallIcon(android.R.drawable.ic_menu_close_clear_cancel)
+            .build()
         startForeground(1, foregroundNotification)
         // TODO: read *Period from storage
         receiver = object : BroadcastReceiver() {
